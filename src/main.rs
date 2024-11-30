8e72a8ea399d2d7503062d9ffda4ab8e607740c3
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-11-30 18:13:39 +0100

    Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,125 @@
-use std::collections::HashSet;
-use nostr_sdk::prelude::*;
+fn main() {
+    panic!("Run `cargo test`");
+}
 
-const RELAY_URL: &str = "wss://relay.damus.io";
-const PUBLIC_KEY: &str = "npub1drvpzev3syqt0kjrls50050uzf25gehpz9vgdw08hvex7e0vgfeq0eseet"; // Yuki public key
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+    use std::sync::LazyLock;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    // Sync and query events
-    let lmdb_events = lmdb().await?;
-    let ndb_events = ndb().await?;
+    use nostr_sdk::prelude::*;
+    use tokio::sync::OnceCell;
 
-    // Collect IDs
-    let lmdb_output_ids: Vec<String> = lmdb_events.into_iter().map(|e| e.id.to_hex()).collect();
-    let ndb_output_ids: Vec<String> = ndb_events.into_iter().map(|e| e.id.to_hex()).collect();
+    const RELAY_URL: &str = "wss://relay.damus.io";
+    static PUBLIC_KEY: LazyLock<PublicKey> = LazyLock::new(|| {
+        PublicKey::parse("npub1drvpzev3syqt0kjrls50050uzf25gehpz9vgdw08hvex7e0vgfeq0eseet").unwrap()
+    });
+    static SYNC_FILTER: LazyLock<Filter> = LazyLock::new(|| {
+        Filter::new()
+            .author(*PUBLIC_KEY)
+            .kind(Kind::TextNote)
+            .until(Timestamp::from_secs(1732980781))
+    });
 
-    if lmdb_output_ids.len() != ndb_output_ids.len() {
-        println!("Query output len NOT match:");
-        println!("- LMDB: {}", lmdb_output_ids.len());
-        println!("- NDB: {}", ndb_output_ids.len());
+    static LMDB_CLIENT: OnceCell<Client> = OnceCell::const_new();
+    static NDB_CLIENT: OnceCell<Client> = OnceCell::const_new();
 
-        // Convert to hashset
-        let set1: HashSet<String> = lmdb_output_ids.iter().cloned().collect();
-        let set2: HashSet<String> = ndb_output_ids.iter().cloned().collect();
+    async fn setup<T>(database: T) -> Result<Client>
+    where
+        T: IntoNostrDatabase,
+    {
+        // Construct client
+        let client: Client = Client::builder().database(database).build();
 
-        let difference: HashSet<_> = set1.symmetric_difference(&set2).collect();
+        // Add relay and connect
+        client.add_relay(RELAY_URL).await?;
+        client.connect().await;
 
-        println!("Difference:");
-        for id in difference {
-            println!("- {}", id);
-        }
-    } else {
-        println!("Query output len match");
+        // Negentropy reconcile
+        client
+            .sync(SYNC_FILTER.clone(), &SyncOptions::default())
+            .await?;
+
+        Ok(client)
     }
 
-    // Expected IDs
-    // abc5a7ed07d60de9e5eb21da34c8369af21e04455c9d3930bd122b2e049d9791
-    // f2d71a515ce3576d238aaaeaa48fde97388162d08208f729b540a4c3f9723e6b
-    // 670303f9cbb24568c705b545c277be1f5172ad84795cc9e700aeea5bb248fd74
+    async fn lmdb() -> &'static Client {
+        LMDB_CLIENT
+            .get_or_init(|| async {
+                let database = NostrLMDB::open("nostr-lmdb").unwrap();
+                setup(database).await.unwrap()
+            })
+            .await
+    }
 
-    // Output nostrdb
-    // fae111325f4c4030c214fa2d2eb6020acf3961d142c922d270a8eb45eabd81e4
-    // fde4199cf10077baa9f08d146f20d5a7a1e62696749f9f2d69459e134c0480e3
-    // fc3eb4ad9359733dc55b5a3c5835c29eb5f47fb11ab7f536e8040191f46f6811
+    async fn ndb() -> &'static Client {
+        NDB_CLIENT
+            .get_or_init(|| async {
+                let database = NdbDatabase::open("nostr-ndb").unwrap();
+                setup(database).await.unwrap()
+            })
+            .await
+    }
 
-    assert_eq!(lmdb_output_ids, ndb_output_ids);
+    fn assert_events(lmdb: Events, ndb: Events) {
+        // Collect IDs
+        let lmdb_ids: Vec<String> = lmdb.into_iter().map(|e| e.id.to_hex()).collect();
+        let ndb_ids: Vec<String> = ndb.into_iter().map(|e| e.id.to_hex()).collect();
 
-    Ok(())
-}
+        // Check len
+        if lmdb_ids.len() != ndb_ids.len() {
+            println!("Difference number of events:");
+            println!("LMDB: {}", lmdb_ids.len());
+            println!("NDB: {}", ndb_ids.len());
 
-async fn lmdb() -> Result<Events> {
-    let database = NostrLMDB::open("nostr-lmdb")?;
-    run(database).await
-}
+            // Convert to hashset
+            let set1: HashSet<String> = lmdb_ids.into_iter().collect();
+            let set2: HashSet<String> = ndb_ids.into_iter().collect();
 
-async fn ndb() -> Result<Events> {
-    let database = NdbDatabase::open("nostr-ndb")?;
-    run(database).await
-}
+            // hashset difference
+            let difference: HashSet<_> = set1.symmetric_difference(&set2).collect();
 
-async fn run<T>(database: T) -> Result<Events>
-where
-    T: IntoNostrDatabase,
-{
-    // Construct client
-    let client: Client = Client::builder()
-        .database(database)
-        .build();
+            println!("Difference IDs:");
+            for id in difference {
+                println!("- {}", id);
+            }
 
-    // Add relay and connect
-    client.add_relay(RELAY_URL).await?;
-    client.connect().await;
+            panic!("Query output len NOT match");
+        }
 
-    // Parse
-    let public_key: PublicKey = PublicKey::parse(PUBLIC_KEY)?;
+        assert_eq!(lmdb_ids, ndb_ids, "rust-nostr LMDB implementation and nostrdb return different in outputs");
+    }
 
-    // Negentropy reconcile
-    let filter = Filter::new().author(public_key).kind(Kind::TextNote).until(Timestamp::from_secs(1732980781));
-    client
-        .sync(filter, &SyncOptions::default())
-        .await?;
+    async fn run_test(query_filter: Filter) {
+        let lmdb_client = lmdb().await;
+        let ndb_client = ndb().await;
 
-    // Query events from database
-    let filter = Filter::new().author(public_key).kind(Kind::TextNote).limit(3);
-    println!("Filter: {}", filter.as_json());
-    let events = client.database().query(vec![filter]).await?;
+        let lmdb_events = lmdb_client
+            .database()
+            .query(vec![query_filter.clone()])
+            .await
+            .unwrap();
+        let ndb_events = ndb_client
+            .database()
+            .query(vec![query_filter])
+            .await
+            .unwrap();
 
-    Ok(events)
+        assert_events(lmdb_events, ndb_events);
+    }
+
+    #[tokio::test]
+    async fn query_by_author_and_kind() {
+        let query_filter = Filter::new().author(*PUBLIC_KEY).kind(Kind::TextNote);
+        run_test(query_filter).await;
+    }
+
+    #[tokio::test]
+    async fn query_by_author_kind_and_limit() {
+        let query_filter = Filter::new()
+            .author(*PUBLIC_KEY)
+            .kind(Kind::TextNote)
+            .limit(3);
+        run_test(query_filter).await;
+    }
 }
